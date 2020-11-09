5347aca3ff4843dbb9002ad2084fd9dd10dbcd37
Dac Van <van264661@nuce.edu.vn>
2020-11-09 23:05:35 +0700

    Add project files.

---
+++ BlogWebsite/EF/CategoryDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BlogWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWebsite.EF
+{
+    public class CategoryDbContext:DbContext
+    {
+        public CategoryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity => {
+                entity.HasIndex(p => p.Slug);
+            });
+        }
+    }
+}
---
+++ BlogWebsite/Models/Category.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWebsite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //Category cha
+        [DisplayName("Danh mục cha")]
+        public int? ParentId { get; set; }
+
+        //Tieu de Category
+        [Required(ErrorMessage = "Phải có tên danh mục")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} dài {1} đến {2}")]
+        [Display(Name = "Tên danh mục")]
+        public string Title { get; set; }
+
+        //Noi dung chi tiet ve Category
+        [DataType(DataType.Text)]
+        [Display(Name = "Nội dung của danh mục")]
+        public string Content { get; set; }
+
+        //Chuoi Url
+        [Required(ErrorMessage = "Phải tạo url")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} dài {1} đến {2}")]
+        [RegularExpression(@"^[a-z0-9-]*$", ErrorMessage = "Chỉ dùng các ký tự [a-z0-9-]")]
+        [DisplayName("Url hiển thị")]
+        public string Slug { get; set; }
+
+
+        //Cac Category con
+        public ICollection<Category> CategoryChildren { get; set; }
+
+        [ForeignKey("ParentId")]
+        public Category ParentCategory { set; get; }
+    }
+}
